e1e16441aac91021b087240787f71a6c80897c43
macielf1994 <macielf1994@gmail.com>
2022-01-10 12:34:02 -0300

    adicionando type anotation para o dicionario

--- exec_job.py
+++ exec_job.py
@@ -1,12 +1,13 @@
+from typing import Mapping
 from exec_querys.create_tables import exec_create_tables
 from exec_querys.generete_bases import generate_files_bases
 from mmids import list_mmids
 
 
-closing_config = {
+closing_config: Mapping[str, int, list] = {
     'version' : '1',
     'range_start_date' : '2021-12-01',
-    'range_end_date' : '2021-12-31',
+    'range_end_date' : '2021-12-30',
     'mmids' : list_mmids
 }
 
